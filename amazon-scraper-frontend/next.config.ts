6758cef966675182f3a8d457f4095abfe15dffdb
Kaique Geraldo <kaique.candido@aluno.senai.br>
2025-05-13 13:06:11 -0300

    ajuste no frontend

--- amazon-scraper-frontend/next.config.ts
+++ amazon-scraper-frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/amazon-scraper",
+  assetPrefix: "/amazon-scraper/",
 };
 
 export default nextConfig;
